3c5d818890e11f74b12ac28806591df368054f91
root <root@ip-172-31-26-196.us-east-2.compute.internal>
2022-02-10 18:07:32 +0000

    sending file

---
+++ main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region = "us-east-2"
+  access_key = "AKIAVC7XZSLVTUO55GN7"
+  secret_key = "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"
+}
+
+
---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {
+  default = "172.16.21.0/24"
+}
+
+variable "tenancy" {
+  default = "dedicated"
+}
+
+variable "subnet_cidr" {
+  default = "172.16.21.0/25"
+}
+
